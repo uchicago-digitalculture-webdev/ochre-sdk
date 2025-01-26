1c0ee072b7ac4b84eb302b3d1e6be78bede139fa
Firat Ciftci <2052855+firatciftci@users.noreply.github.com>
2025-01-25 21:18:02 -0500

    Update changeset config, update tests

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,22 +1,24 @@
 import { expect, it } from "vitest";
 import { fetchWebsite } from "./utils/fetchers/website.js";
 
-it("website", async () => {
-  const website = await fetchWebsite("ospama");
+const [ospama, guerrillaTelevision, uchicagoNode] = await Promise.all([
+  fetchWebsite("ospama"),
+  fetchWebsite("guerrilla-television"),
+  fetchWebsite("uchicago-node"),
+]);
 
-  expect(website?.identification.label).toBe(
+it("website", async () => {
+  expect(ospama?.identification.label).toBe(
     "Gender and Politics in Early Modern European Republics (Venice, Genoa XV-XVIII centuries)",
   );
 });
 
 it("website with sidebar", async () => {
-  const website = await fetchWebsite("guerrilla-television");
-
-  expect(website?.sidebarElements.length).toBeGreaterThan(0);
+  expect(guerrillaTelevision?.sidebarElements.length).toBeGreaterThan(0);
 });
 
 it("website with page with css styles", async () => {
-  const website = await fetchWebsite("uchicago-node");
-
-  expect(website?.pages[0]?.properties.cssStyles.length).toBeGreaterThan(0);
+  expect(uchicagoNode?.pages[0]?.properties.cssStyles.length).toBeGreaterThan(
+    0,
+  );
 });
