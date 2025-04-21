93bbc418f680cbe521996ab5668bfa4627faa285
Firat Ciftci <2052855+firatciftci@users.noreply.github.com>
2025-04-21 12:37:20 -0500

    fix: actually bring back gallery fetcher

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./types/main.js";
+export * from "./utils/fetchers/gallery.js";
 export * from "./utils/fetchers/item.js";
 export * from "./utils/fetchers/uuid.js";
 export * from "./utils/getters.js";
