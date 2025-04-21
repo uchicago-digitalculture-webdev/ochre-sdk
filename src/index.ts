e77a3688de7a96c18e596cab625e35a1f44525be
Firat Ciftci <2052855+firatciftci@users.noreply.github.com>
2025-04-21 12:42:44 -0500

    fix: export website fetcher

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from "./types/main.js";
 export * from "./utils/fetchers/gallery.js";
 export * from "./utils/fetchers/item.js";
 export * from "./utils/fetchers/uuid.js";
+export * from "./utils/fetchers/website.js";
 export * from "./utils/getters.js";
 export * from "./utils/parse.js";
 export * from "./utils/string.js";
