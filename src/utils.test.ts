9acd0a50c7aee3427a48274cf0dcb64fd1852049
Firat Ciftci <2052855+firatciftci@users.noreply.github.com>
2025-05-17 01:50:28 -0400

    Fixes

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,23 +1,14 @@
-import { writeFileSync } from "node:fs";
 import { expect, it } from "vitest";
 import { fetchItem } from "./utils/fetchers/item.js";
 import { fetchWebsite } from "./utils/fetchers/website.js";
 
-const [
-  ospama,
-  guerrillaTelevision,
-  uchicagoNode,
-  { item: rasShamraData },
-  { item: zincirliData },
-] = await Promise.all([
-  fetchWebsite("ospama"),
-  fetchWebsite("guerrilla-television"),
-  fetchWebsite("uchicago-node"),
-  fetchItem("ff48d967-f69c-48e6-aed3-4e03e52ed51d", "set"),
-  fetchItem("e0f0153e-82a2-4930-89f1-5f58b7da0f16", "spatialUnit"),
-]);
-
-writeFileSync("zincirli.json", JSON.stringify(zincirliData, null, 2));
+const [ospama, guerrillaTelevision, uchicagoNode, { item: rasShamraData }] =
+  await Promise.all([
+    fetchWebsite("ospama"),
+    fetchWebsite("guerrilla-television"),
+    fetchWebsite("uchicago-node"),
+    fetchItem("ff48d967-f69c-48e6-aed3-4e03e52ed51d", "set"),
+  ]);
 
 it("website", async () => {
   expect(ospama?.identification.label).toBe(
