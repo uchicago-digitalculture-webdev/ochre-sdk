41fdec7a49971d2b88a706b71e084026f7a20086
Firat Ciftci <2052855+firatciftci@users.noreply.github.com>
2025-06-10 00:14:10 -0400

    test: enable website test for ospama

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -3,22 +3,22 @@ import { fetchItem } from "./utils/fetchers/item.js";
 import { fetchWebsite } from "./utils/fetchers/website.js";
 
 const [
-  // [_, ospama],
+  [_, ospama],
   [__, guerrillaTelevision],
   [___, uchicagoNode],
   { item: rasShamraData },
 ] = await Promise.all([
-  // fetchWebsite("ospama"),
+  fetchWebsite("ospama"),
   fetchWebsite("guerrilla-television"),
   fetchWebsite("uchicago-node"),
   fetchItem("ff48d967-f69c-48e6-aed3-4e03e52ed51d", "set"),
 ]);
 
-// it("website", async () => {
-//   expect(ospama?.identification.label).toBe(
-//     "Gender and Politics in Early Modern European Republics (Venice, Genoa XV-XVIII centuries)",
-//   );
-// });
+it("website", async () => {
+  expect(ospama?.identification.label).toBe(
+    "Gender and Politics in Early Modern European Republics (Venice, Genoa XV-XVIII centuries)",
+  );
+});
 
 it("website with sidebar", async () => {
   expect(guerrillaTelevision?.sidebar?.elements.length).toBeGreaterThan(0);
