8ccaa849513d1fc21e60eccdf197eca901e20859
Firat Ciftci <2052855+firatciftci@users.noreply.github.com>
2025-11-06 13:32:42 -0500

    chore: update package.json and ESLint config for improved module exports and remove yaml support

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import antfu from "@antfu/eslint-config";
 export default antfu({
   stylistic: false,
   markdown: false,
-  yaml: false,
   typescript: {
     parserOptions: {
       tsconfigRootDir: import.meta.dir,
