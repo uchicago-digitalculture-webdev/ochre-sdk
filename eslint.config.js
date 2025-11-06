5f1149dbab7a3a5cd86b18cb9a714c75b7532949
Firat Ciftci <2052855+firatciftci@users.noreply.github.com>
2025-11-06 13:06:17 -0500

    chore: update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import antfu from "@antfu/eslint-config";
 export default antfu({
   stylistic: false,
   markdown: false,
+  yaml: false,
   typescript: {
     parserOptions: {
       tsconfigRootDir: import.meta.dir,
