59ad843ae14e8f23e11aefb2f9ae5de786e4efd5
Firat Ciftci <2052855+firatciftci@users.noreply.github.com>
2026-02-05 17:19:03 -0500

    refactor: remove items-by-uuids-and-links fetcher

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export * from "./types/main.js";
 export * from "./utils/fetchers/gallery.js";
 export * from "./utils/fetchers/item.js";
 export * from "./utils/fetchers/items-by-property-values.js";
-export * from "./utils/fetchers/items-by-uuids-and-links.js";
 export * from "./utils/fetchers/property-values-by-property-variables.js";
 export * from "./utils/fetchers/website.js";
 export * from "./utils/getters.js";
