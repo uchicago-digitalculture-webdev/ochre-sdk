b92144419d5354c45651b951adf303866a6a6087
Firat Ciftci <2052855+firatciftci@users.noreply.github.com>
2026-05-08 13:27:23 -0500

    feat: export MultilingualString and related types

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,13 @@ export * from "#/fetchers/set/property-values.js";
 export * from "#/fetchers/website.js";
 export * from "#/getters.js";
 export * from "#/helpers.js";
+export { MultilingualString } from "#/parsers/multilingual.js";
+export type {
+  MultilingualOptions,
+  MultilingualStringEntry,
+  MultilingualStringInput,
+  MultilingualStringJSON,
+  MultilingualStringText,
+} from "#/parsers/multilingual.js";
 export * from "#/types/index.js";
 export * from "#/types/website.js";
